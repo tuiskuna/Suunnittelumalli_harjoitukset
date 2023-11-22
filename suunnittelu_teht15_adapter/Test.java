a303878fea3a44e2bc64a6481d22868fd9b727f6
tuiskuna <104358542+tuiskuna@users.noreply.github.com>
2023-11-22 11:14:44 +0200

    Update Test.java

--- suunnittelu_teht15_adapter/Test.java
+++ suunnittelu_teht15_adapter/Test.java
@@ -2,10 +2,13 @@ public class Test {
     public static void main(String[] args) {
 
         OldService oldService = new OldService();
+        //tänne kans se ITarget
         Adapter adapter = new Adapter(oldService);
 
         ClientClass client = new ClientClass(adapter);
 
+        //voidaan lisätä kanssa uusia servicejä joita ei käytetän adapterin kautta vaan ne menee suoraan Itarget rajapinnan kautta
+        
         int sum = client.requestService();
         System.out.println("The converted sum: " + sum);
     }
